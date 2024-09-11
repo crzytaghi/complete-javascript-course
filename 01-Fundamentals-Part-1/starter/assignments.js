2c0b47f257e4d24125d9a3d64a7b3f4c81147291
Andrew Taghi <aataghi13@gmail.com>
2024-09-11 12:34:40 -0500

    added html script and assignments.js file

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,15 @@
+// Values and Variables
+let country = "USA";
+let continent = "North America";
+let population = 300;
+
+console.log({ country, continent, population, isIsland });
+
+// Data Types
+const isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof language);
+
+// Switch Statements
