3fd49a913ec6e25e9880568f094ef20c89c28b10
Andrew Taghi <aataghi13@gmail.com>
2024-09-13 11:06:45 -0500

    added functions to key in javascript object

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,11 @@
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  },
+};
+
+console.log(john.calcBMI());
